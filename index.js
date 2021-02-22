a4280cac1c683dd15725c23b112bbd1ea8d5db36
Anjan Roy <anjanroy@yandex.com>
2021-02-22 18:33:58 +0530

    new: given block number, get block hash

--- index.js
+++ index.js
@@ -43,3 +43,56 @@ const getLatestBlockHeader = async _ => {
     }
 
 }
+
+// Given block number, get block hash
+const fetchBlockHashByNumber = num => {
+
+    try {
+
+        const blockHeader = await axios.post(HTTPURI,
+            {
+                "id": 1,
+                "jsonrpc": "2.0",
+                "method": "chain_getBlockHash",
+                "params": [num]
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+
+        return 'result' in blockHeader.data ? blockHeader.data.result : null
+
+    } catch (e) {
+
+        console.error(e.toString())
+        return null
+
+    }
+
+}
+
+const processBlocksInRange = (x, y) => {
+
+    for (let i = x; i <= y; i++) {
+
+
+
+    }
+
+}
+
+// Main entry point function, to be invoked for starting light client ops
+const main = _ => {
+
+    let lastSeenBlock = BigInt(0)
+
+    while (1) {
+
+
+
+    }
+
+}
