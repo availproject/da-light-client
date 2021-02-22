e0bffecc966f5fde1b89c5ed3b1c784704c116f9
Anjan Roy <anjanroy@yandex.com>
2021-02-22 18:05:56 +0530

    new: js verifier script, interfaces with rust code over FFI

---
+++ verifier.js
@@ -0,0 +1,23 @@
+const ffi = require('ffi-napi')
+const ref = require('ref-napi')
+const array = require('ref-array-di')(ref)
+
+const U8Array = array(ref.types.uint8)
+
+// Creating interface to be used for calling verifier
+const lib = ffi.Library('libverifier', {
+    verify_proof: ['bool', ['uint8', 'uint8', U8Array, 'size_t', U8Array, 'size_t']],
+})
+
+module.exports = {
+
+    verifyProof: (row, col, commitment, proof) => {
+
+        const _commitment = new U8Array(commitment)
+        const _proof = new U8Array(proof)
+
+        return lib.verify_proof(row, col, _commitment, _commitment.length, _proof, _proof.length)
+
+    }
+
+}
