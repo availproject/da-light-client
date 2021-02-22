ba063b84763db1ddb7fe39f04e75beeb016edce0
Anjan Roy <anjanroy@yandex.com>
2021-02-22 18:21:08 +0530

    new: get latest block header

--- index.js
+++ index.js
@@ -14,3 +14,32 @@ const getRandomInt = (low, high) => {
     return Math.floor(Math.random() * (high - low + 1)) + low
 
 }
+
+// Query for latest block header, in case of failure returns `null`
+const getLatestBlockHeader = async _ => {
+
+    try {
+
+        const blockHeader = await axios.post(HTTPURI,
+            {
+                "id": 1,
+                "jsonrpc": "2.0",
+                "method": "chain_getHeader"
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+
+        return 'result' in blockHeader.data ? blockHeader.data.result : null
+
+    } catch (e) {
+
+        console.error(e.toString())
+        return null
+
+    }
+
+}
