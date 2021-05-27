6231317604dfce7129e84919b6254d80ca69cc48
Anjan Roy <anjanroy@yandex.com>
2021-05-27 11:05:58 +0530

    new: oracle interface

---
+++ onchain/contracts/IDAOracle.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IDAOracle {
+    function requestConfidence(uint256 block_) external;
+    function confidence(uint256 block_) external returns (uint256, uint256, uint256, bool);
+}
